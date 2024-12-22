caee0b79ffd1627dcfc0efc8e2aceef199247467
linanw <linanwang@msn.com>
2024-12-22 22:16:08 +0800

    Update cam list

--- components/Videocall.tsx
+++ components/Videocall.tsx
@@ -21,7 +21,7 @@ const Videocall = (props: { session_name: string; JWT: string }) => {
   const client = useRef<typeof VideoClient>(ZoomVideo.createClient());
   const camPanelDefaultHeight = 360;
   const [session, setSession] = useState(undefined);
-  const [camIdList, setCamIdList] = useState<string[]>(["linanw-cnc_cam0", "linanw-cnc_cam1", "default_cam0", "default_cam1", "default_cam2", "default_cam3",
+  const [camIdList, setCamIdList] = useState<string[]>(["pty-lct-law-pan-5_cam0", "pty-lct-law-pan-5_cam1", "default_cam0", "default_cam1", "default_cam2", "default_cam3",
     "default_cam4", "default_cam5", "default_cam6", "default_cam7"]);
   const [currentTimeoutId, setCurrentTimeoutId] = useState<number>(0);
   const [zoomMultiplier, setZoomMultiplier] = useState(0);
