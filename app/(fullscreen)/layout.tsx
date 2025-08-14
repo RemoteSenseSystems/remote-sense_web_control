3da7080f9c890f3b55130419780e29febed55caa
linanw <linanwang@msn.com>
2025-08-14 15:05:09 +0800

    Enable PWA

--- app/(fullscreen)/layout.tsx
+++ app/(fullscreen)/layout.tsx
@@ -9,10 +9,17 @@ export default function RootLayout({
 }) {
     return (
         <html suppressHydrationWarning lang="en">
-            <head />
+            <head>
+                <title>RemoteSense Systems</title>
+                <meta name="mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="apple-touch-fullscreen" content="yes" />
+                <meta name="apple-mobile-web-app-title" content="React Navigation" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+            </head>
             <body
                 className={clsx(
-                    "min-h-screen bg-background font-sans antialiased",
+                    "min-h-screen bg-black font-sans antialiased",
                     fontSans.variable,
                 )}
             >
